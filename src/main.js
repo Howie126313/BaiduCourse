882593d86dc85a6ca64dccc798b2e1f2be8b5084
wanghaoyu <howie126313@gmail.com>
2018-05-09 10:45:52 +0800

    2.2条件与循环

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import san, {DataTypes} from 'san'
 
-import {router} from 'san-router'
+import {router, link} from 'san-router'
 
 import Hello from './component/Hello.san'
+import Mission2_2 from './component/Mission2_2.san'
 
 router.add({rule: '/', Component: Hello, target: '#root'})
+router.add({rule: '/mission2_2', Component: Mission2_2, target: '#root'})
 
 router.start()
