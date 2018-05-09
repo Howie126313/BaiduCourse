8afbc4c82d278ad3219d8432c0682d3f82a052ef
wanghaoyu <howie126313@gmail.com>
2018-05-09 11:52:41 +0800

    2.3 样式

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import {router, link} from 'san-router'
 
 import Hello from './component/Hello.san'
 import Mission2_2 from './component/Mission2_2.san'
+import Mission2_3 from './component/Mission2_3.san'
 
 router.add({rule: '/', Component: Hello, target: '#root'})
 router.add({rule: '/mission2_2', Component: Mission2_2, target: '#root'})
+router.add({rule: '/mission2_3', Component: Mission2_3, target: '#root'})
 
 router.start()
