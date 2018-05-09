9b636d30776cd23ffc6c93d4c47856c45feff87e
wanghaoyu <howie126313@gmail.com>
2018-05-09 15:16:42 +0800

    2.4

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Get Start
 
 项目使用 webpack4 和 san 构建
-
+使用与 vue 相似的代码风格实现
 
 ```
 yarn install
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import san, {DataTypes} from 'san'
 
 import {router, link} from 'san-router'
 
-import Hello from './component/Hello.san'
-import Mission2_2 from './component/Mission2_2.san'
-import Mission2_3 from './component/Mission2_3.san'
+import Hello from './pages/Hello.san'
+import Mission2_2 from './pages/Mission2_2.san'
+import Mission2_3 from './pages/Mission2_3.san'
+import Mission2_4 from './pages/Mission2_4.san'
 
 router.add({rule: '/', Component: Hello, target: '#root'})
 router.add({rule: '/mission2_2', Component: Mission2_2, target: '#root'})
 router.add({rule: '/mission2_3', Component: Mission2_3, target: '#root'})
+router.add({rule: '/mission2_4', Component: Mission2_4, target: '#root'})
 
 router.start()
